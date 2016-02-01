c375c4e6b21f355fd31f4b4c2e2235dd65bd7b52
Artyom Korkhov <artyom.korkhov@gmail.com>
2016-02-01 11:37:56 +0300

    base logging config

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,37 @@ created by artemkorkhov at 2016/01/16
 """
 
 import os
+import logging.config
 
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+logging_conf = {
+    'version': 1,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s\t%(name)s\t%(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'level': 'DEBUG'
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG'
+    },
+    'loggers': {
+        '__main__': {
+            'level':'DEBUG',
+            # 'handlers':['console']
+            'propagate': True
+        },
+    }
+}
+
+
+logging.config.dictConfig(logging_conf)
